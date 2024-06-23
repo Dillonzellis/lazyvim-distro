c93efb344521d5fced03ed52366dc3f321df7a94
dillonzellis <dillonzellis@gmail.com>
2024-06-23 12:07:38 -0400

    initial lazyvim distro setup

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,8 @@
+return {
+  -- disable default plugins
+  -- { "echasnovski/mini.pairs", enabled = false },
+  { "akinsho/bufferline.nvim", enabled = false },
+  { "goolord/alpha-nvim", enabled = false },
+  -- { "nvim-pack/nvim-spectre", enabled = false },
+  -- { "nvim-lualine/lualine.nvim", enabled = false },
+}
