e0162c751a0cc40f013fcb5177d4fe7b4b2d3e86
Dillon Ellis <31318165+Dillonzellis@users.noreply.github.com>
2024-06-27 10:31:20 -0400

    add gruvbox with transparent bg

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,23 @@
+return {
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   opts = {
+  --     transparent = true,
+  --     styles = {
+  --       sidebars = "transparent",
+  --       floats = "transparent",
+  --     },
+  --   },
+  -- },
+
+  { "ellisonleao/gruvbox.nvim", opts = {
+    transparent_mode = true,
+  } },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+}
