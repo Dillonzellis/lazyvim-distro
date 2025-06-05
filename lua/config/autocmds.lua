81a155133c73b1c972f93820dec3bfc2e0667038
Dillon Ellis <31318165+Dillonzellis@users.noreply.github.com>
2025-06-05 13:09:13 -0400

    feat: autocmd for .env filetype

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,11 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Set .env files to dotenv filetype (not sh)
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = { "*.env", ".env*", ".env" },
+  callback = function()
+    vim.bo.filetype = "dotenv"
+  end,
+})
