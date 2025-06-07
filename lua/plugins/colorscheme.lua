ea5956901f4680ac553125b75e43d5d01f59690a
dillonzellis <dillonzellis@gmail.com>
2025-06-07 17:20:33 -0400

    feat: colorschemes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,11 +2,11 @@ return {
   {
     "folke/tokyonight.nvim",
     opts = {
-      -- transparent = true,
-      -- styles = {
-      --   sidebars = "transparent",
-      --   floats = "transparent",
-      -- },
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
     },
   },
   {
@@ -15,23 +15,23 @@ return {
       transparent_mode = true,
     },
   },
-  -- {
-  --   "lunarvim/synthwave84.nvim",
-  --   config = function()
-  --     require("synthwave84").setup({
-  --       glow = {
-  --         error_msg = true,
-  --         type2 = true,
-  --         func = true,
-  --         keyword = true,
-  --         operator = false,
-  --         buffer_current_target = true,
-  --         buffer_visible_target = true,
-  --         buffer_inactive_target = true,
-  --       },
-  --     })
-  --   end,
-  -- },
+  {
+    "lunarvim/synthwave84.nvim",
+    config = function()
+      require("synthwave84").setup({
+        glow = {
+          error_msg = true,
+          type2 = true,
+          func = true,
+          keyword = true,
+          operator = false,
+          buffer_current_target = true,
+          buffer_visible_target = true,
+          buffer_inactive_target = true,
+        },
+      })
+    end,
+  },
   {
     "EdenEast/nightfox.nvim",
     config = function()
@@ -45,26 +45,38 @@ return {
       })
     end,
   },
-  -- {
-  --   "mistweaverco/retro-theme.nvim",
-  --   opts = {
-  --     italic_comments = true,
-  --     disable_cache = false,
-  --     hot_reload = false,
-  --   },
-  -- },
-  -- {
-  --   "scottmckendry/cyberdream.nvim",
-  --   lazy = false,
-  --   priority = 1000,
-  --   opts = {
-  --     transparent = true,
-  --   },
-  -- },
+  {
+    "neanias/everforest-nvim",
+  },
+  {
+    "rebelot/kanagawa.nvim",
+  },
+  {
+    "thesimonho/kanagawa-paper.nvim",
+    -- lazy = false,
+    -- priority = 1000,
+    opts = {},
+  },
+  {
+    "mistweaverco/retro-theme.nvim",
+    opts = {
+      italic_comments = true,
+      disable_cache = false,
+      hot_reload = false,
+    },
+  },
+  {
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      transparent = true,
+    },
+  },
   {
     "LazyVim/LazyVim",
     opts = {
-      -- colorscheme = "catppuccin",
+      colorscheme = "terafox",
     },
   },
 }
